ca5553f4d584019fb338eb1a8b3b62747fabd6e0
XurifyTeine <xurifyteine@gmail.com>
2023-05-30 12:29:06 -0400

    Enabled SVGs in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
   async headers() {
     return [
       {

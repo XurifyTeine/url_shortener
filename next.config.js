2eb76073f06141f488da45a5b56a7b7eb5a26800
XurifyTeine <xurifyteine@gmail.com>
2023-05-31 21:30:48 -0400

    Updated

--- next.config.js
+++ next.config.js
@@ -19,26 +19,6 @@ const nextConfig = {
             key: 'X-DNS-Prefetch-Control',
             value: 'on'
           },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'same-origin'
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'clipboard-read=self,clipboard-write=self,web-share=self,cross-origin-isolated=self',
-          },
         ]
       }
     ]

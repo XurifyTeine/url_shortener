e65284a48fc380245e3d705db6636acb1c629fa0
XurifyTeine <xurifyteine@gmail.com>
2023-05-31 21:12:45 -0400

    Testing headers

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'"
+            value: "default-src 'self'; script-src 'self'; connect-src 'unsafe-inline'"
           },
           {
             key: 'Referrer-Policy',

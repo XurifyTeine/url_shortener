7500a913674eb04648c1f87ecf5eeef083fd4dea
XurifyTeine <xurifyteine@gmail.com>
2023-05-31 21:01:52 -0400

    Updated headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,47 @@ const nextConfig = {
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'none'; script-src 'self'"
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(); battery=(); geolocation=(); microphone=()',
+          },
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;

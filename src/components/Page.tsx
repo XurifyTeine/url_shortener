9f9155242c30d89a368827bc3e1e270709b950b8
XurifyTeine <xurifyteine@gmail.com>
2023-06-02 08:54:10 -0400

    Testing Vercel API with Go

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Head from 'next/head';
+import Head from "next/head";
 
 import GitHubLink from "@/src/components/GitHubLink";
 import ToastNotification from "@/src/components/ToastNotification";
@@ -7,27 +7,25 @@ import { useToast } from "@/src/context/ToastContext";
 import ErrorBoundary from "./ErrorBoundary";
 
 export const Page: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { state: toastState } = useToast();
 
   React.useEffect(() => {
-    fetch(`/api/next-go-healthz`)
+    fetch(`/api/next-go-healthz`).catch((error) => {
+      console.error(error);
+    });
   }, []);
 
-  const { state: toastState } = useToast();
   return (
     <>
-     <Head>
-        <title>
-          NoLongr - URL Shortener
-        </title>
+      <Head>
+        <title>NoLongr - URL Shortener</title>
         <meta
           name="description"
           content="This is a simple tool made for the simple purpose of shortening URLs!"
           key="desc"
         />
       </Head>
-      <ErrorBoundary name="global">
-        {children}
-      </ErrorBoundary>
+      <ErrorBoundary name="global">{children}</ErrorBoundary>
       <GitHubLink />
       {toastState && (
         <ToastNotification

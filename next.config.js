8cdeeaced6c44b7c243de9ca0b6622aa896d2c91
XurifyTeine <xurifyteine@gmail.com>
2023-06-03 20:45:25 -0400

    Testing headers

--- next.config.js
+++ next.config.js
@@ -7,46 +7,55 @@ const nextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   async headers() {
+    const ContentSecurityPolicy = `
+      default-src 'self';
+      script-src 'self';
+      child-src 'self';
+      style-src 'self';
+      font-src 'self';
+      connect-src 'self' vitals.vercel-insights.com;
+    `;
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
           {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
           },
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'same-origin'
+            key: "Referrer-Policy",
+            value: "same-origin",
           },
           {
-            key: 'Permissions-Policy',
-            value: 'clipboard-read=self,clipboard-write=self,web-share=self,cross-origin-isolated=self',
+            key: "Permissions-Policy",
+            value:
+              "clipboard-read=self,clipboard-write=self,web-share=self,cross-origin-isolated=self",
           },
           {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; connect-src vitals.vercel-insights.com*; child-src 'none';"
-          }
-        ]
-      }
-    ]
-  }
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

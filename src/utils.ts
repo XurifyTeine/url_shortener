e205da2cf3f5a643211483d187b64f04d432442e
XurifyTeine <xurifyteine@gmail.com>
2023-06-04 01:06:20 -0400

    Fixed bug with getIdFromPathname util

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 export const getIdFromPathname = (pathname: string): string | null => {
   if (pathname && pathname.slice(0, 5) === "/urls") {
     const numberOfSlashes = pathname.split("/").filter((item) => item).length;
-    if (numberOfSlashes !== 1) return null;
-    const id = pathname.split("/")[1];
+    if (numberOfSlashes !== 2) return null;
+    const id = pathname.split("/")[2];
     return id;
   }
   return null;

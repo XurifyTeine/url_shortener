c7feaf423198e015fab9aee139263500beb2ac4f
XurifyTeine <xurifyteine@gmail.com>
2023-06-03 20:33:06 -0400

    Added back headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,43 @@ const nextConfig = {
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'clipboard-read=self,clipboard-write=self,web-share=self,cross-origin-isolated=self',
+          },
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;

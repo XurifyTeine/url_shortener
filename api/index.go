e47289a8db652cee06db6810e2946ff42d962084
XurifyTeine <xurifyteine@gmail.com>
2023-06-10 13:48:03 -0400

    Test

--- api/index.go
+++ api/index.go
@@ -3,14 +3,8 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
-func Handler() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }

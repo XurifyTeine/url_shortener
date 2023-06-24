3a0c2d6abc7974183a49be8f0a8ac19245ef2d30
XurifyTeine <xurifyteine@gmail.com>
2023-06-24 12:30:04 -0400

    Cleaned up Icons location

---
+++ src/components/Icons/QRCodeIcon.tsx
@@ -0,0 +1,23 @@
+export const QRCodeIcon = () => {
+  return (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="800"
+    height="800"
+    fill="#FFFFFF"
+    viewBox="0 0 24 24"
+    className="w-6 h-6"
+  >
+    <path
+      fill="#FFFFFF"
+      d="M12 13v4a1 1 0 01-1 1H7a1 1 0 01-1-1v-4a1 1 0 011-1h4a1 1 0 011 1zm-5-3h2a1 1 0 001-1V7a1 1 0 00-1-1H7a1 1 0 00-1 1v2a1 1 0 001 1zm10 4h-2a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 00-1-1zm0-8h-4a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V7a1 1 0 00-1-1z"
+    ></path>
+    <path
+      fill="#FFFFFF"
+      d="M21 9a1 1 0 01-1-1V4h-4a1 1 0 010-2h4a2 2 0 012 2v4a1 1 0 01-1 1zm1 11v-4a1 1 0 00-2 0v4h-4a1 1 0 000 2h4a2 2 0 002-2zM4 8V4h4a1 1 0 000-2H4a2 2 0 00-2 2v4a1 1 0 002 0zm5 13a1 1 0 00-1-1H4v-4a1 1 0 00-2 0v4a2 2 0 002 2h4a1 1 0 001-1z"
+    ></path>
+  </svg>
+  );
+};
+
+export default QRCodeIcon;

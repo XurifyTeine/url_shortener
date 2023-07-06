7a70b64fba0c9e999cf3db6a73627036abc4a039
XurifyTeine <xurifyteine@gmail.com>
2023-07-06 08:08:02 -0400

    Updated

--- src/components/StackableToasts.tsx
+++ src/components/StackableToasts.tsx
@@ -11,7 +11,7 @@ export const StackableToasts: React.FC<StackableToastsProps> = ({
   if (!Array.isArray(toastsState)) return null;
   if (toastsState.length < 1) return null;
   return (
-    <div className="flex flex-col items-end absolute bg-transparent top-0 left-0 w-full max-h-1">
+    <div className="flex flex-col items-end absolute bg-transparent top-0 left-0 w-full max-h-1 fixed">
       {toastsState.map((toast) => (
         <ToastNotification
           message={toast.message}
